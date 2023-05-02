f25d00167f19415f8d9fdba58bcbbf4f8166f3fe
Mikkel Petersen <mlp2305@gmail.com>
2023-05-02 22:12:18 +0200

    add buttonbuilder to prelude

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod style_bundle_builder;
 pub mod text_bundle_builder;
 
 pub mod prelude {
+    pub use crate::button_bundle_builder::*;
     pub use crate::node_bundle_builder::*;
     pub use crate::style_bundle_builder::*;
     pub use crate::text_bundle_builder::*;
